c8629e659cd6bca12a37de495c8f6507367fc094
Samuel Catley <scp94373@gmail.com>
2020-01-06 07:17:14 +0000

    Time Date update
    Day is .. next explorer session is <not complete>

--- src/App.js
+++ src/App.js
@@ -2,87 +2,85 @@ import React from 'react';
 import './main.css';
 import explorerLogo from './media/Explorers.png';
 import scoutLogo from './media/Scouts.png';
+import Body from './Body/Body'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="jumbotron text-center">
-        <div className="row">
-          <div className="col-sm-6">
-          <img className="explorerImg" src={explorerLogo} />
-          </div>
-          <div className="col-sm-6">
-          <img className="scoutImg" src={scoutLogo} />
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <h1><b>Goudhurst Explorers</b></h1>
-            <br />
-          </div>
-          <div className="col-sm-12 text-center">
-          <p>
-            Explorers are the next <s>troop</s> <u>unit</u> after Scouts, our age group ranges from 14 - 18 years old. We meet on Friday evenings between 7 - 9PM.
-          </p>
-          </div>
-          <div className="col-sm-12">
-          <h3>A little bit about us from the scout website</h3>
-          <p>
-            With the support, direction and guidance of Unit leaders, Explorer Scouts are encouraged to lead themselves, design their own programme and work towards the top awards that Scouting offers. 
-            With exciting prospects like being a part of camps and expeditions both home and abroad; adventurous activities such as mountaineering, parascending and off shore sailing; Explorers offers fun and adventure for all. 
-            Explorers also have the opportunity to be a part of The Explorer Scout Young Leaders’ Scheme which develops their leadership skills and sense of responsibility, by helping to run meetings for younger sections.
-            </p>
-        </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12">
-            <h3>Schedule</h3>
-            <br />
-            <table>
-              <tr>
-                <th>22/11/19</th>
-                <td>Fundraising</td>
-                <td>Notes: We will be creating things that can be sold during "Late night shopping"</td>
-              </tr>
-              <tr>
-                <th>29/11/19</th>
-                <td>Games</td>
-                <td>Notes:</td>
-              </tr>
-              <tr>
-                <th>06/12/19</th>
-                <td>Mince Pies, Christmas Decorations, Christmas themed games</td>
-                <td>Notes: Please bring along any ingrediants/ideas/resources for christmas related things!</td>
-              </tr>
-              <tr>
-                <th>13/12/19</th>
-                <td><strong>Jump In @ Tonbridge</strong></td>
-                <td>Notes: <strong>7PM - 9PM</strong>, Please drop off and pick up your explorer at the below address.<br/>
-                Address: <strong>2, Morley Rd, Tonbridge TN9 1RA</strong></td>
-              </tr>
-            </table>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12">
-            <br />
-            <p>If you have any questions please don't hesitate to ask, please click <a href="mailto:goudhurstexplorergroup@hotmail.com">here</a> to email us.</p>
-          </div>
-        </div> 
-        <div className="row">
-          <div className="col-sm-12">            
-            <br/>
-            <i>
-              This website is currently in development and will likely change mutliple times. 
-              However if you wish to get updates on the website with regards to major content change, please <a href="mailto:goudhurstexplorergroup@hotmail.com?subject=Subscription for website updates">email by clicking here</a> with a subject of "Subscription for website updates".
-              </i>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+class App extends React.Component {
+  
+  currentDay = "";
+  d = new Date();
+  editableDate = new Date();
+  currentDayNumber = this.d.getDay();
+  friday = 5;
+  firstFriday;
+  secondFriday;
+  thirdFriday;
+  fourthFriday;
+  fifthFriday;
+  latestDateResult;
+  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  updateDay() {
+    console.log('updateDay this.d')
+    console.log(this.d)
+    this.currentDay = this.days[this.d.getDay()];
+  }
+
+  findDates(dayNumber, date) {
+    let diff = date.getDay() - dayNumber;
+    if (diff > 0) {
+        date.setDate(date.getDate() + 6);
+    }
+    else if (diff < 0) {
+      date.setDate(date.getDate() + ((-1) * diff))
+    }
+    else {
+      date.setDate(date.getDate() + 7);
+    }
+    return date;
+    // console.log('findDates _date')
+    // console.log(_date)
+    
 }
 
+componentDidMount() {
+  this.updateDates();
+}
+  updateDates() {
+    this.firstFriday = this.findDates(this.friday, this.editableDate);
+    console.log('this.firstFriday')
+    console.log(this.firstFriday)
+    this.secondFriday = this.findDates(this.friday, this.firstFriday)
+    console.log('this.secondFriday')
+    console.log(this.secondFriday)
+    this.thirdFriday = this.findDates(this.friday, this.secondFriday);
+    console.log('this.thirdFriday')
+    console.log(this.thirdFriday)
+    this.fourthFriday = this.findDates(this.friday, this.thirdFriday);
+    console.log('this.fourthFriday')
+    console.log(this.fourthFriday)
+    // this.fifthFriday = this.findDates(this.friday, this.fourthFriday);
+    // console.log('this.fifthFriday')
+    // console.log(this.fifthFriday)
+  }
+  render() {
+    
+  
+    this.updateDay();
+    // this.updateDates()
+    return (
+      <div className="App">
+        <Body 
+          getTimeDate={this.currentDay}
+          firstFriday={this.firstFriday}
+        />
+      </div>
+    );
+  
+  }
+  
+}
+function getTimeDate() {
+  let d = new Date();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days[d.getDay()]
+}
 export default App;
