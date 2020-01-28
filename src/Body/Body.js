4e7544cd80d1e38a67eb32b86864b2071c2b2471
Samuel Catley <scp94373@gmail.com>
2020-01-28 20:17:09 +0000

    Update Schedule

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -57,28 +57,58 @@ const Body = props => {
             <table>
               <tbody>
                 <tr>
-                  {/* <th>{props.firstFridayDate} {props.firstFridayMonth}</th> */}
-                  <th>17th January</th>
-                  <td>Scheduling night</td>
-                  <td>Notes: Will be planning the rest of the term.</td>
-                </tr>
-                <tr>
-                  {/* <th>{props.secondFridayDate} {props.secondFridayMonth}</th> */}
-                  <th>24th January</th>
-                  <td>Games</td>
-                  <td>Notes: Games night! If you want to bring any games, feel free!</td>
-                </tr>
-                <tr>
                   {/* <th>{props.thirdFridayDate} {props.thirdFridayMonth}</th> */}
                   <th>31st January</th>
-                  <td>Camping - sponsored - unconfirmed</td>
-                  <td>Notes: Will speak further about this on the 24th January.</td>
+                  <td>Badge planning night</td>
+                  <td>Notes: Quiet evening, planning short term and long term badge goals.</td>
                 </tr>
                 <tr>
                   {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
                   <th>7th February</th>
                   <td>Lordship Woods - Wide games - unconfirmed</td>
                   <td>Notes: Seb - Snacks, Theo - Activities</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>14th February</th>
+                  <td>Pancakes & Pancakes themed activities</td>
+                  <td>Notes: Making Pancakes, Oliver / Bea - Ingrediants</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>21st February</th>
+                  <td>Half term</td>
+                  <td>Notes: Half term</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>28th February</th>
+                  <td>Games</td>
+                  <td>Notes: Lizzie / Sam - Game ideas</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>6th March</th>
+                  <td>Climbing</td>
+                  <td>Notes: Chimera - transport discussed sooner.</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>13th March</th>
+                  <td>Quiz Night (Fundraising)</td>
+                  <td>Notes: Will, Seb, Fin to organise.</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>20th March</th>
+                  <td>Hike - Sponsered?</td>
+                  <td>Notes: Lizzie - Route / Equipment</td>
+                </tr>
+                <tr>
+                  {/* <th>{props.fourthFridayDate} {props.fourthFridayMonth}</th> */}
+                  <th>27th March</th>
+                  <td>International Cooking</td>
+                  <td>Notes: Everyone to bring their own ingrediants, night to be planned closer to the time.</td>
                 </tr>
               </tbody>
             </table>
